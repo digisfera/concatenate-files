73258da13a89e5b867fd02b03d749b024ddbfb34
Manuel Cabral <m.cabral@digisfera.pt>
2014-02-01 17:35:17 +0000

    Removed coffeescript in tests

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ module.exports = function(inputFiles, outputFile, options, callback) {
   options = options || {};
 
   filerw.readFilesUtf8(inputFiles, function(err, filesData) {
+    if(err) { return callback(err); }
     separator = options.separator || '';
 
     var res = null;
---
+++ test/test.js
@@ -0,0 +1,38 @@
+var expect = require('chai').expect,
+    fs = require('fs'),
+    rimraf = require('rimraf'),
+    concatFun = require('../index');
+
+describe('concat-file-fun', function() {
+
+  before(function() {
+    rimraf.sync( __dirname + "/tmp");
+  });
+
+  readFileUtf8 = function(f) { return fs.readFileSync(f, { encoding: 'utf8'})};
+  
+  it('should concatenate files', function(done) {
+    var inFile1 = __dirname + "/files/f1.txt",
+        inFile2 = __dirname + "/files/f2.txt",
+        outFile = __dirname + "/tmp/concat01.txt";
+    concatFun([inFile1, inFile2], outFile, { separator: '.' }, function(err, result) {
+      expect(err).to.be.not.ok;
+      expect(result).to.equal(outFile);
+      expect(readFileUtf8(outFile)).to.equal('foo.bar');
+      done();
+    });
+  });  
+  it('callback error if one of the files does not exist', function(done) {
+    var inFile1 = __dirname + "/files/f1.txt",
+        inFileInvalid = 'unexisting file',
+        inFile2 = __dirname + "/files/f2.txt",
+        outFile = __dirname + "/tmp/concat02.txt";
+    concatFun([inFile1, inFileInvalid, inFile2], outFile, { separator: '.' }, function(err, result) {
+      expect(err).to.be.ok;
+      expect(result).to.be.not.ok;
+      expect(readFileUtf8.bind(null, outFile)).to.throw();
+      done();
+    });
+  });
+    
+});
